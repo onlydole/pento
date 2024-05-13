39ea8fad77f87a7dbf8d10b79a77c8e5e1b3b055
Taylor Dolezal <onlydole@gmail.com>
2024-05-13 09:58:47 -0700

    End of Chapter 4 content
    Signed-off-by: Taylor Dolezal <onlydole@gmail.com>

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -4,29 +4,31 @@ defmodule PentoWeb.WrongLive do
 
   def mount(_params, session, socket) do
     {
-      :ok, 
+      :ok,
       assign(
-        socket, 
-        score: 0, 
+        socket,
+        score: 0,
         message: "Make a guess:"
       )
-  }
+    }
   end
 
-  @spec render(any)::Phoenix.LiveView.Rendered.t()
+  @spec render(any) :: Phoenix.LiveView.Rendered.t()
   def render(assigns) do
     ~H"""
     <h1 class="mb-4 text-4xl font-extrabold">Your score: <%= @score %></h1>
     <h2>
-      <%= @message %>
-      It's <%= time() %>
+      <%= @message %> It's <%= time() %>
     </h2>
-    <br/>
+    <br />
     <h2>
       <%= for n <- 1..10 do %>
-        <.link class="bg-blue-500 hover:bg-blue-700
+        <.link
+          class="bg-blue-500 hover:bg-blue-700
         text-white font-bold py-2 px-4 border border-blue-700 rounded m-1"
-        phx-click="guess" phx-value-number= {n} >
+          phx-click="guess"
+          phx-value-number={n}
+        >
           <%= n %>
         </.link>
       <% end %>
@@ -41,17 +43,18 @@ defmodule PentoWeb.WrongLive do
   def handle_event("guess", %{"number" => guess}, socket) do
     message = "Your guess: #{guess}. Wrong. Guess again."
     score = socket.assigns.score - 1
+
     {
-      :noreply, 
+      :noreply,
       assign(
-        socket, 
-        score: score, 
+        socket,
+        score: score,
         message: message
       )
     }
   end
 
   def time() do
-    DateTime.utc_now |> to_string
+    DateTime.utc_now() |> to_string
   end
 end
